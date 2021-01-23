aca96418132968ea8b3a6fb200194d1447309048
sonallux <13821543+sonallux@users.noreply.github.com>
2021-01-23 11:05:26 +0100

    Rename PostgresRepository into PostgresClient
    Fixes #5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export { PostgresRepository } from './postgresRepository'
+import { PostgresClient } from './postgresClient'
+
+// Export `PostgresClient` also under its old name `PostgresRepository`, so users can update gradually
+export { PostgresClient, PostgresClient as PostgresRepository }
